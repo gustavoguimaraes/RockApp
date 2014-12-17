601686f3d7e54e0bb1c5c5be801c2f6dbf6fa413
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-12-17 18:59:32 +0100

    View Ember. Tough lesson

--- js/app.js
+++ js/app.js
@@ -74,6 +74,42 @@ App.ArtistsSongsRoute = Ember.Route.extend({
 
 });
 
+//Views
+//Views have access to their context
+App.StarRatingView = Ember.View.extend({
+  templateName: 'star-rating',
+  classNames: ['rating-panel'],
+
+  rating: Ember.computed.alias('content.rating'),
+  numStars:  Ember.computed.alias('maxRating'),
+  fullStars: Ember.computed.alias('rating'),
+
+  stars: function() {
+    var ratings = [];
+    var fullStars = this.starRange(1, this.get('fullStars'), 'full');
+    var emptyStars = this.starRange(this.get('fullStars') + 1, this.get('numStars'), 'empty');
+    Array.prototype.push.apply(ratings, fullStars);
+    Array.prototype.push.apply(ratings, emptyStars);
+    return ratings;
+  }.property('fullStars', 'numStars'),
+
+  starRange: function(start, end, type) {
+    var starsData = [];
+    for (var i = start; i <= end; i++) {
+      starsData.push({ rating: i, full: type === 'full' });
+    }
+    return starsData;
+  },
+
+  actions: {
+    setRating: function() {
+      var newRating = Ember.$(event.target).data('rating');
+      this.set('rating', newRating);
+    }
+  }
+
+});
+
 // App.SongCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
 //  sortProperties: ['rating'],
 //  sortAscending: false,
@@ -84,17 +120,17 @@ App.ArtistsSongsRoute = Ember.Route.extend({
 App.Songs = Ember.A();
 
 //Led Zeppelin
-App.Songs.pushObject(App.Song.create({title: 'Black Dog', artist: 'Led Zeppelin', rating: 8}));
-App.Songs.pushObject(App.Song.create({title: 'Kashmir', artist: 'Led Zeppelin', rating: 7}));
-App.Songs.pushObject(App.Song.create({title: 'My Love', artist: 'Led Zeppelin', rating: 6}));
+App.Songs.pushObject(App.Song.create({title: 'Black Dog', artist: 'Led Zeppelin', rating: 2}));
+App.Songs.pushObject(App.Song.create({title: 'Kashmir', artist: 'Led Zeppelin', rating: 3}));
+App.Songs.pushObject(App.Song.create({title: 'My Love', artist: 'Led Zeppelin', rating: 4}));
 
 //Radiohead
-App.Songs.pushObject(App.Song.create({title: 'Karma Police', artist: 'Radiohead', rating: 10}));
-App.Songs.pushObject(App.Song.create({title: 'House of Cards', artist: 'Radiohead', rating: 11}));
-App.Songs.pushObject(App.Song.create({title: 'Lotus', artist: 'Radiohead', rating: 9}));
+App.Songs.pushObject(App.Song.create({title: 'Karma Police', artist: 'Radiohead', rating: 5}));
+App.Songs.pushObject(App.Song.create({title: 'House of Cards', artist: 'Radiohead', rating: 5}));
+App.Songs.pushObject(App.Song.create({title: 'Lotus', artist: 'Radiohead', rating: 3}));
 
 //Sivelchair
-App.Songs.pushObject(App.Song.create({title: 'Cemetery', artist: 'Silverchair', rating: 7}));
+App.Songs.pushObject(App.Song.create({title: 'Cemetery', artist: 'Silverchair', rating: 2}));
 App.Songs.pushObject(App.Song.create({title: 'Fat Boy', artist: 'Silverchair', rating: 4}));
 App.Songs.pushObject(App.Song.create({title: 'Miss You', artist: 'Silverchair', rating: 5}));
 
